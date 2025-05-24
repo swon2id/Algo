3343c04d93d70d15f3cdcdb5f9e08af9c8bbc6a4
Lee Sangwon (Lee Hanwool) <83111797+swon2id@users.noreply.github.com>
2025-05-24 19:17:10 +0900

    [Silver II] Title: 좌표 압축, Time: 1752 ms, Memory: 264456 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.StringTokenizer;
@@ -12,31 +12,30 @@ public class Main {
     private static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
-        br.readLine();
+        int N = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
 
-        ArrayList<Integer> list = new ArrayList<>();
-        ArrayList<Integer> sortedList = new ArrayList<>();
-        while(st.hasMoreTokens()) {
-            int elem = Integer.parseInt(st.nextToken());
-            list.add(elem);
-            sortedList.add(elem);
+        int[] list = new int[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = Integer.parseInt(st.nextToken());
         }
 
-        sortedList.sort(Comparator.naturalOrder());
+        int[] sortedList = list.clone();
+        Arrays.sort(sortedList);
         HashMap<Integer, Integer> orderTable = new HashMap<>();
 
         int compressedIndex = 0;
         for (int e: sortedList) {
-            if (orderTable.containsKey(e)) continue;
-            orderTable.put(e, compressedIndex++);
+            if (!orderTable.containsKey(e)) {
+                orderTable.put(e, compressedIndex++);
+            }
         }
 
         for (int e: list) {
             sb.append(orderTable.get(e)).append(" ");
         }
 
-        sb.setLength(sb.length()-1);
+        sb.setLength(sb.length() - 1);
         System.out.print(sb);
     }
 }
