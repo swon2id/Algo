50290308267c8c5cb7857f895a77cbdb25d86533
Lee Sangwon (Lee Hanwool) <83111797+swon2id@users.noreply.github.com>
2025-05-26 13:56:45 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 2032 ms, Memory: 124356 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+	static HashMap<String, Integer> memo = new HashMap<>();
+    public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = null;
+		StringBuilder sb = new StringBuilder();
+		
+		while ((input = br.readLine()) != null) {
+			StringTokenizer st = new StringTokenizer(input);
+			int a, b, c;
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			c = Integer.parseInt(st.nextToken());
+			
+			if (b == c && b == a && b == -1) break;
+			sb.append(String.format("w(%d, %d, %d) = %d\n", a, b, c, w(a, b, c)));
+		}
+		br.close();
+		sb.setLength(sb.length() - 1);
+		System.out.print(sb);
+    }
+	
+	public static int w(int a, int b, int c) {
+		if (a <= 0 || b <= 0 || c <= 0) return 1;
+		if (a > 20 || b > 20 || c > 20) return w(20, 20, 20);
+		
+		String memoKey = String.format("%d %d %d", a, b, c);
+		if (memo.containsKey(memoKey)) {
+			return memo.get(memoKey);
+		}
+		
+		if (a < b && b < c) {
+			memo.put(memoKey, w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c));
+			return memo.get(memoKey);
+		}
+		memo.put(memoKey, w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1));
+		return memo.get(memoKey);
+	}
+}
